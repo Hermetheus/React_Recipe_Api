b9918edfb4003b7bad521d3ddfab2dd0e38c92d4
Allan Olson <allanmolson@gmail.com>
2019-07-02 21:11:30 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import Recipes from "./components/Recipes";
+
+const API_KEY = "789111266c6e6c472d513ecca42c7829";
+
+class App extends Component {
+  state = {
+    recipes: []
+  };
+
+  getRecipe = async e => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(
+      `https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}`
+    );
+    const data = await api_call.json();
+
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
+      </div>
+    );
+  }
+}
+
+export default App;
